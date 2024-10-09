dd264947f3fab411ed6a05dd732fe3c87b72d98c
DengelLionel <dengelriveracajincho@gmail.com>
2024-10-09 01:43:30 -0500

    ok

--- components/CursosSlide.vue
+++ components/CursosSlide.vue
@@ -1,32 +1,56 @@
 <template>
-  <swiper
-    class="container-swiper-slidder"
-    :slides-per-view="3"
-    :autoplay="{
-      delay: 3000,
-      disableOnInteraction: false
-    }"
-  >
-    <swiper-slide class="slide-curso" v-for="i in 9" :key="i">
-      <img
-        class="imagen-curso-slide"
-        src="/images/imagen1c.png"
-        alt="Metodo villegas"
-      />
-    </swiper-slide>
-  </swiper>
+  <div class="relative container-swiper-slidder">
+
+    <!-- Swiper Slider -->
+    <swiper
+      :modules="[Navigation, Pagination, Scrollbar]" 
+      :slides-per-view="3"
+      :loop="true"
+      :autoplay="{
+        delay: 3000,
+        disableOnInteraction: false
+      }"
+      navigation
+      :scrollbar="{ draggable: true }"
+    >
+      <swiper-slide class="slide-curso" v-for="i in 9" :key="i">
+        <img
+          class="imagen-curso-slide"
+          src="/images/imagen1c.png"
+          alt="Metodo villegas"
+        />
+      </swiper-slide>
+
+  
+    </swiper>
+  </div>
 </template>
 
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import 'swiper/swiper-bundle.css';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';  // Importar los módulos necesarios
+import 'swiper/swiper-bundle.css';  // Importar el CSS necesario
 </script>
 
 <style scoped>
 /* Estilos generales del contenedor */
 .container-swiper-slidder {
-  overflow: hidden;  /* Asegura que no haya contenido fuera del contenedor */
-  width: 100%;  /* El contenedor del slider ocupa todo el ancho */
+  overflow: hidden;
+  width: 100%;
+  position: relative; /* Para que las flechas estén posicionadas dentro del contenedor */
+}
+
+/* Estilos de las flechas */
+.swiper-button-next:after, .swiper-rtl .swiper-button-prev:after {
+  color: white !important;
+}
+
+.swiper-button-prev {
+  left: 10px;
+}
+
+.swiper-button-next {
+  right: 10px;
 }
 
 /* Ajustes para dispositivos pequeños */
@@ -34,11 +58,11 @@ import 'swiper/swiper-bundle.css';
   .imagen-curso-slide {
     width: 220px;
     height: auto;
-    object-fit: cover;  /* Mantiene las proporciones de la imagen */
+    object-fit: cover;
   }
   .slide-curso {
     width: 220px !important;
-    margin-right: 20px;  /* Ajusta el margen */
+    margin-right: 20px;
     display: flex;
     justify-content: center;
   }
@@ -48,14 +72,14 @@ import 'swiper/swiper-bundle.css';
 @media (min-width: 769px) {
   .slide-curso {
     width: 320px !important;
-    margin-right: 30px;  /* Ajusta el margen */
+    margin-right: 30px;
     display: flex;
     justify-content: center;
   }
   .imagen-curso-slide {
     width: 320px;
     height: auto;
-    object-fit: cover;  /* Mantiene las proporciones de la imagen */
+    object-fit: cover;
   }
 }
 </style>
