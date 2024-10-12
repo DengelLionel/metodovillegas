3e9504029ba21bddedbb727af1c118335d714621
DengelLionel <dengelriveracajincho@gmail.com>
2024-10-11 20:58:35 -0500

    ok

--- components/PieLanding.vue
+++ components/PieLanding.vue
@@ -4,12 +4,13 @@
   <img class="w-[150px]" src="@/assets/images/logometodovillegas.webp" alt="Logo metodo villegas">
   <h2 class="ml-3 text-xl text-white" >Método Villegas</h2>
   <div class="text-white mt-[20px] mb-[20px] flex flex-col gap-[10px]">
-    <div class="flex flex-row gap-[7px] align-center">
- <Instagram/> <span class="text-xl lg:text-2xl">@equipartumente  </span>
-    </div>
-   <div class="flex flex-row gap-[7px] align-center">
-  <Tiktok/> <span class="text-xl lg:text-2xl">@equipartumente  360  </span>
-   </div>
+  
+        <a class="flex flex-row gap-[7px] align-center" target="_blank" href="https://www.instagram.com/equipartumente/"><Instagram/> <span class="text-xl lg:text-2xl">@equipartumente  </span></a>
+ 
+
+    <a class="flex flex-row gap-[7px] align-center" target="_blank" href="https://www.tiktok.com/@equipartumente360?_t=8qT8zr63Kqs&_r=1"> <Tiktok/> <span class="text-xl lg:text-2xl">@equipartumente  360  </span></a>
+ 
+  
   
   </div>
         </div>
