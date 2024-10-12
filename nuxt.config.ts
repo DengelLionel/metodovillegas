ff802c3662eb489c23f772907266cf66ee050639
DengelLionel <dengelriveracajincho@gmail.com>
2024-10-12 16:58:03 -0500

    ok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,38 @@ export default defineNuxtConfig({
         {
           rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
         }
-      ]
+      ],
+      script: [
+        {
+          hid: 'facebook-pixel',
+          innerHTML: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1298547537801212');  // Reemplaza con tu Pixel ID real
+            fbq('track', 'PageView');
+          `,
+          type: 'text/javascript',
+          charset: 'utf-8'
+        }
+      ],
+      noscript: [
+        {
+          hid: 'facebook-pixel-noscript',
+          innerHTML: `<img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1298547537801212&ev=PageView&noscript=1"/>`,
+          body: true
+        }
+      ],
+      __dangerouslyDisableSanitizersByTagID: {
+        'facebook-pixel': ['innerHTML'],
+        'facebook-pixel-noscript': ['innerHTML']
+      }
     }
   },
   postcss: {
