a111f3ab7ec5518070c904c5eeaad04e795be4fb
DengelLionel <dengelriveracajincho@gmail.com>
2024-10-12 16:13:18 -0500

    f

--- pages/manifestacion.vue
+++ pages/manifestacion.vue
@@ -90,38 +90,7 @@ definePageMeta({
       link: [
         { rel: 'me', href: 'https://www.instagram.com/equipartumente' },
         { rel: 'me', href: 'https://www.tiktok.com/@equipartumente360?_t=8qT8zr63Kqs&_r=1' }
-      ],
-      script: [
-        {
-          hid: 'facebook-pixel',
-          innerHTML: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window,document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1298547537801212'); 
-            fbq('track', 'PageView');
-          `,
-          type: 'text/javascript',
-          charset: 'utf-8'
-        }
-      ],
-      noscript: [
-        {
-          hid: 'facebook-pixel-noscript',
-          innerHTML: `<img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=1298547537801212&ev=PageView&noscript=1"/>`,
-          body: true
-        }
-      ],
-      __dangerouslyDisableSanitizersByTagID: {
-        'facebook-pixel': ['innerHTML'],
-        'facebook-pixel-noscript': ['innerHTML']
-      }
+      ]
     }
   }
 })
