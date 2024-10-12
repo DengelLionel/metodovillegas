b556c0d502453f74b0aeb642a10b1894e8a94ba0
DengelLionel <dengelriveracajincho@gmail.com>
2024-10-12 16:47:06 -0500

    ok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,24 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  app:{
-  head:{
-    link:[
-      {
-        rel:'stylesheet',href:'https://fonts.googleapis.com/css2?family=Kurale&family=Protest+Strike&display=swap'
-      },
-      {
-        rel:'stylesheet',href:'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
-      }
-    ]
-  }
-},
+  app: {
+    head: {
+      title: 'Merezco la Vida de Mis Sueños',  // Título predeterminado
+      titleTemplate: '%s | Merezco la Vida de Mis Sueños',  // Template con sufijo
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Transforma tu vida en menos de un mes con técnicas universales.' }
+      ],
+      link: [
+        {
+          rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Kurale&family=Protest+Strike&display=swap'
+        },
+        {
+          rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap'
+        }
+      ]
+    }
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
